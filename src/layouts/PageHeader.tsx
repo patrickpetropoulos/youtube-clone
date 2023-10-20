9049bbce2f4fae99ee1bd003f6055681880f1d53
patrickpetropoulos <33436629+patrickpetropoulos@users.noreply.github.com>
2023-10-19 22:11:58 -0400

    At 19:28 minute mark

---
+++ src/layouts/PageHeader.tsx
@@ -0,0 +1,20 @@
+import { Menu } from "lucide-react";
+import logo from "../assets/Logo.png";
+import { Button } from "../components/Button";
+
+export function PageHeader() {
+  return (
+    <div className="flex gap-10 lg:gap-20 justify-between ">
+      <div className="flex gap-4 items-center flex-shrink-0 ">
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
+        <a href="/">
+          <img src={logo} className="h-6" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
